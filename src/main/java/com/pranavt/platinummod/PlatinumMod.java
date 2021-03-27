1a84d3e0d7a6568335a323d480e7123d807c04cd
awsame303 <tpranav2009@gmail.com>
2021-03-27 12:00:37 -0400

    Added platinum item

--- src/main/java/com/pranavt/platinummod/PlatinumMod.java
+++ src/main/java/com/pranavt/platinummod/PlatinumMod.java
@@ -1,36 +1,29 @@
 package com.pranavt.platinummod;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
+
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
-@Mod("pranavtplatinummod")
-public class 	PlatinumMod
-{
-    public static final Logger LOGGER = LogManager.getLogger();
+import com.pranavt.platinummod.core.init.ItemInit;
 
-    public PlatinumMod() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        MinecraftForge.EVENT_BUS.register(this);
-    }
+@Mod(PlatinumMod.MOD_ID)
+public class PlatinumMod {
+	
+	public static final Logger LOGGER = LogManager.getLogger();
+	public static final String MOD_ID = "pranavtplatinummod";
 
-    private void setup(final FMLCommonSetupEvent event)
-    {
-    }
+	public PlatinumMod() {
+		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+		bus.addListener(this::setup);
+		ItemInit.ITEMS.register(bus);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	private void setup(final FMLCommonSetupEvent event) {
+	}
 }
-
-
-
--- src/main/java/com/pranavt/platinummod/core/init/ItemInit.java
+++ src/main/java/com/pranavt/platinummod/core/init/ItemInit.java
@@ -1,5 +1,18 @@
 package com.pranavt.platinummod.core.init;
 
+import com.pranavt.platinummod.PlatinumMod;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
 public class ItemInit {
 
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,
+			PlatinumMod.MOD_ID);
+
+	public static final RegistryObject<Item> TITANIUM = ITEMS.register("platinum",
+			() -> new Item(new Item.Properties().tab(ItemGroup.TAB_MATERIALS)));
 }
